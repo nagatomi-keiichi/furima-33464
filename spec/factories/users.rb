abfc0c248131e8ff130cf08ebd3ba1c8a64afac6
nagatomikeiichi <nagatomi.keiichi@gmail.com>
2021-02-02 18:43:29 +0900

    ユーザー管理機能の実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    first_name {"てすと"}
+    last_name {"てすと"}
+    first_name_kana {"テスト"}
+    last_name_kana {"テスト"}
+    birthday_id {"2000-01-01"}
+  end
+end
