cf83cd37986c9ae84aa305fc18cfd9e9a51631f2
nagatomikeiichi <nagatomi.keiichi@gmail.com>
2021-02-06 16:03:51 +0900

    コメント修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品ができる場合' do
-    context 'ユーザー新規登録ができる場合' do
+    context '商品が出品できる場合' do
       it 'nameとinfomationとpriceとcategory_idとstate_idとburden_idとarea_idとday_idとuser_idとimageがあると保存できる' do
         expect(@item).to be_valid
       end
