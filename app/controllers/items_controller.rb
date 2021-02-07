3a455b78a314cfc03aa05aea682c628b2af27166
nagatomikeiichi <nagatomi.keiichi@gmail.com>
2021-02-07 20:35:42 +0900

    コメントアウトの削除 商品詳細ページに戻るように変更 before_actionに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @item = Item.all.order('id DESC')
   end
@@ -18,16 +19,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to action: :index unless current_user == @item.user
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -35,8 +33,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  private
   def item_params
     params.require(:item).permit(:name, :infomation, :category_id, :state_id, :burden_id, :area_id, :day_id, :price,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
