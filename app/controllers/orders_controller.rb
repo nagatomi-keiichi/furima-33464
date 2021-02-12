d55eacedcc735f11624c9ac902743162f8962c9f
nagatomikeiichi <nagatomi.keiichi@gmail.com>
2021-02-12 15:49:40 +0900

    URLを直接入力して売却済み商品の商品購入ページへ遷移しようとすると、トップページに遷移するを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :set_order
-  before_action :move_to_index, only: [:index]
+  before_action :move_to_index, only: [:index,:create]
 
   def index
     @order_address = OrderAddress.new
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id == @item.user.id
+    redirect_to root_path if current_user.id == @item.user.id || @item.order
   end
 
   def pay_item
